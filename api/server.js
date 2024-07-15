f812c47a1da98bf5a61afc0654513182879864a1
pabloparedesmx <107523694+pabloparedesmx@users.noreply.github.com>
2024-07-15 17:06:41 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -22,19 +22,4 @@ app.get('https://x8ki-letl-twmt.n7.xano.io/api:8WqDlSfR/midjourney_sref_explorer
     res.status(500).json({ error: 'An error occurred while fetching the SREF', details: error.message });
   }
 });
-
-app.get('/api/all-srefs', async (req, res) => {
-    try {
-      const response = await fetch(XANO_API_URL);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      res.json(data);
-    } catch (error) {
-      console.error('Error fetching from Xano:', error);
-      res.status(500).json({ error: 'An error occurred while fetching SREFs' });
-    }
-  });
-
 module.exports = app;
